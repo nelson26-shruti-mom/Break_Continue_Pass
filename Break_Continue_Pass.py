02ef38df187f0f4a35b6b983c537fe4c9f319b18
Nelson <62866867+nelson26-shruti-mom@users.noreply.github.com>
2020-05-18 14:31:33 +0530

    Basic Of Break, Continue and Pass

---
+++ Break_Continue_Pass.py
@@ -0,0 +1,20 @@
+#Break_Statement
+for i in range(10):
+    if i==3:
+        break
+    print(i)
+print("Bye")
+
+#Continue_Statement
+for i in range(10):
+    if i==3:
+        continue
+    print(i)
+print("Take care")
+
+#Pass_Statement
+for i in range(10):
+    if i==3:
+       pass
+    print(i)
+print("Good Bye")
